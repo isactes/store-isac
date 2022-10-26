f15c3b581e2ccc896063975a6b040098628b8f62
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-26 14:26:12 -0500

    clae 1 - 10

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,25 +1,62 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.css']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  @Input() img: string = '';
+  img: string = '';
+
+  @Input('img') 
+  set changeImg (newImg: string){
+    this.img = newImg;
+    console.log('change just img => ', this.img)
+    // code
+  }
+  @Input() alt: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = 'assets/img/last.jpg';
+
+  // counter = 0;
+  // counterFn: number | undefined;
 
 
   constructor() { 
     // Before render
-    // no usar async -- once time 
-    console.log('constructor', 'imgValue =>', this.img)
+    // no usar async -- any times 
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges (changes: SimpleChanges ){
+    // Before & during render 
+    //  changes inpust -- times 
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+    console.log('change', changes); 
+
   }
 
   ngOnInit(): void {
+    // Before render
+    // async & await, fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+    // this.counterFn =  window.setInterval(() => {
+    //   this.counter += 1;
+    //   console.log('runcounter');
+    // }, 1000);
   }
+
+  ngAfterViewInit(): void {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+   ngOnDestroy(): void {
+    //  delete  
+    console.log('ngOnDestroy');
+    // window.clearInterval(this.counterFn);
+   }
 
   imgError () {
     this.img = this.imageDefault;
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/products.model'; 
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'game 1',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 2,
+      name: 'game 2',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 3,
+      name: 'game 3',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 4,
+      name: 'game 4',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 5,
+      name: 'game 5',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 6,
+      name: 'game ',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 7,
+      name: 'game 7',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 8,
+      name: 'game 8',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+      id: 9,
+      name: 'game 9',
+      image: './assets/img/last.jpg',
+      price: 123,
+      describe: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
