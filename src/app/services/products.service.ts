d2424d6ba09de02b5dae15780b2879e593a9216a
isactes <fidel.renovato@gmail.com>
2022-11-17 23:36:50 -0600

    retry a petition with pipe by url bad

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams  } from '@angular/common/http';
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/products.model';
+import { retry } from 'rxjs/operators';
 
 
 @Injectable({
@@ -20,7 +21,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string) {
